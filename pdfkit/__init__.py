16b6eb8927b26ce46b6e4e976d3716f427c00dd2
Alphamonak Solutions <info@alphamonak.com>
2018-05-07 03:26:42 +0000

    Initial import

---
+++ pdfkit/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Wkhtmltopdf python wrapper to convert html to pdf using the webkit rendering engine and qt
+"""
+
+from .pdfkit import PDFKit
+from .api import from_url, from_file, from_string, configuration
